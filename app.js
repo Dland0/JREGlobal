d4e31d4245906d9791c845bc5b694e709757cddc
Dylan <108112920+Dland0@users.noreply.github.com>
2023-07-20 16:57:44 +1200

    Task4 : Make the output readable

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const csv = require('csvtojson');
 const vd = require('./validation/validators');
 const vdm = require('./validation/validationMaps');
-
 const http = require('http');
+const fs = require('fs');
+const LineError = require('./models/LineError');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Content-Type', 'text/html');
 
   csv().fromFile('./equipment_list.csv').then(
     async (jsonArray) => {
+    let errors = [];
+    await vd.validateEquipment(jsonArray, vdm.equipmentMap, errors);
 
-        let errors = [];
+    let errorHTML = '';
+    for (const error of errors) {
+      errorHTML += error.toHTML();
+    }
 
-        await vd.validateEquipment(jsonArray, vdm.equipmentMap, errors);
-
-        res.end(JSON.stringify(errors))
-        console.log("Endpoint reached.");
-    } 
-  )
-
+    // Read the errors.html template and replace <%= errorHTML %> with the generated error card HTML
+    fs.readFile('errors.html', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading HTML template:', err);
+        res.end('Error reading HTML template');
+      } else {
+        const renderedHTML = data.replace('<%= errorHTML %>', errorHTML);
+        res.end(renderedHTML);
+      }
+    });
+  });
 });
 
 server.listen(port, hostname, () => {
--- models/LineError.js
+++ models/LineError.js
@@ -4,6 +4,17 @@ class LineError {
         this.value = value
         this.line = line
     }
+
+    toHTML() {
+        return `
+          <div class="error-card">
+            <h3>Error Information</h3>
+            <p><strong>Message:</strong> ${this.message}</p>
+            <p><strong>Value:</strong> ${this.value}</p>
+            <p><strong>Line:</strong> ${this.line}</p>
+          </div>
+        `;
+      }
 }
 
 module.exports = LineError
